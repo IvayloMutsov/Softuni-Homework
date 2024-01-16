6ada3fa4e386d61c09a13fc3f5058631b72fdf0e
IvayloMutsov <IGMutsov@gmail.com>
2024-01-16 22:16:23 +0200

    second lecture exercise

---
+++ MultidimensionalArrays exersice/Squares in Matrix.cs
@@ -0,0 +1,35 @@
+﻿namespace _02._Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] info = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = info[0];
+            int cols = info[1];
+
+            char[,] matrix = new char[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                string[] currentRow = Console.ReadLine().Split();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row,col] = char.Parse(currentRow[col]);
+                }
+            }
+
+            int matched = 0;
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row,col] == matrix[row,col + 1] && matrix[row,col] == matrix[row + 1,col] && matrix[row,col] == matrix[row + 1, col + 1])
+                    {
+                        matched++;
+                    }
+                }
+            }
+            Console.WriteLine(matched);
+        }
+    }
+}
