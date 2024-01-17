e79d1461d70368c0a7bf4e87ee2d8178d0b27cdc
IvayloMutsov <IGMutsov@gmail.com>
2024-01-17 19:17:51 +0200

    3rd Lecture

---
+++ Sets and Dictionaries Advanced/Average Student Grades.cs
@@ -0,0 +1,33 @@
+﻿namespace _02._Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentGrades = new Dictionary<string, List<decimal>>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+
+                if (!studentGrades.ContainsKey(name))
+                {
+                    studentGrades.Add(name, new List<decimal>());
+                }     
+                    studentGrades[name].Add(grade);
+            }
+            foreach (var student in studentGrades)
+            {
+                Console.Write($"{student.Key} -> ");
+                foreach (var item in student.Value)
+                {
+                    Console.Write($"{item:f2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced/Count Same Values in Array.cs
@@ -0,0 +1,29 @@
+﻿namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> occurances = new Dictionary<double, int>();
+
+            foreach (var number in numbers) 
+            {
+                if (occurances.ContainsKey(number))
+                {
+                    occurances[number]++;
+                }
+                else
+                {
+                    occurances.Add(number, 1);
+                }
+            }
+
+            foreach (var item in occurances)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
