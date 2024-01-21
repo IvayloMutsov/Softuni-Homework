2750400b1672f88a885d32b3f094ebe4e541cfda
IvayloMutsov <IGMutsov@gmail.com>
2024-01-21 23:51:08 +0200

    Sets and Dictionaries exercise

---
+++ Sets and dictionaries exercise/Sets of Elements.cs
@@ -0,0 +1,29 @@
+﻿namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> n = new();
+            HashSet<int> m = new();
+
+            for (int i = 0; i < input[0]; i++)
+            {
+                n.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < input[1]; i++)
+            {
+                m.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach (var num in n)
+            {
+                if (m.Contains(num))
+                {
+                    Console.Write($"{num} ");
+                }
+            }
+        }
+    }
+}
