b76497adbc228a8b6bd92b6274605186c8f71c6d
IvayloMutsov <IGMutsov@gmail.com>
2024-01-28 15:12:01 +0200

    exersice

---
+++ Functional programming exersice/Predicate for Names.cs
@@ -0,0 +1,16 @@
+﻿namespace _07._Predicate_for_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            Predicate<string> lengthCheck = name => name.Length <= length;
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(n => lengthCheck(n)).ToArray();
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
