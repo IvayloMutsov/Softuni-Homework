c1aaa0793e49cd944ade588c753c012620bd203a
IvayloMutsov <IGMutsov@gmail.com>
2024-02-27 01:27:28 +0200

    .

---
+++ C# OOP/Inheritance/02. Multiple Inheritance/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
