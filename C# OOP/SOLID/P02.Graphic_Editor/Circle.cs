95eb0b87066b248c88dc543e78893508d99b603e
IvayloMutsov <IGMutsov@gmail.com>
2024-03-28 00:24:05 +0200

    .

---
+++ C# OOP/SOLID/P02.Graphic_Editor/Circle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    public class Circle : IShape
+    {
+        public void DrawShape()
+        {
+            Console.WriteLine("I'm Circle");
+        }
+    }
+}
